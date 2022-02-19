b8d0bc59047e7cbc230e021d3d47c5b3a66d5452
Andress Martin <42077892+AndressMartin@users.noreply.github.com>
2022-02-19 10:01:01 -0300

    Fixed animations

--- Assets/CameraMovements.cs
+++ Assets/CameraMovements.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class CameraMovements : MonoBehaviour
 {
+    [SerializeField]
+    float targetedCamSize;
     public bool zoomActive;
+    public bool paningOnNewItem;
     Vector3 target;
     Vector3 originalPos;
     Camera cam;
     public float speed;
-
+    private float smoother;
+    public float smootherMax = .5f;
     private void Start()
     {
         cam = Camera.main;
@@ -20,20 +24,31 @@ public class CameraMovements : MonoBehaviour
     {
         if (zoomActive)
         {
-            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 3, speed);
-            Vector3 closePos = new Vector3(target.x, target.y, target.z - 3);
-            cam.transform.position = Vector3.Lerp(cam.transform.position, closePos, speed);
+            if (paningOnNewItem)
+            {
+                cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetedCamSize, speed*smoother);
+                cam.transform.position = Vector3.Slerp(cam.transform.position, target, speed*smoother);
+                if (smoother <= 1) smoother += 0.01f;
+            }
+            else
+            {
+                cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetedCamSize, speed);
+                cam.transform.position = Vector3.Slerp(cam.transform.position, target, speed);
+            }
+            
         }
         else
         {
             cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 5, speed);
-            cam.transform.position = Vector3.Lerp(cam.transform.position, originalPos, speed);
+            cam.transform.position = Vector3.Slerp(cam.transform.position, originalPos, speed);
         }
     }
 
     public void ZoomIn(GameObject obj)
     {
         zoomActive = !zoomActive;
-        target = obj.transform.position;
+        target = new Vector3(obj.transform.position.x, obj.transform.position.y - 1, obj.transform.position.z - 3);
+        smoother = smootherMax;
+        paningOnNewItem = obj.GetComponent<Interactable>().discovered;
     }
 }
