d2af5f7d496561ae3363cbb2ce8e487fc35ff1f4
Andress Martin <42077892+AndressMartin@users.noreply.github.com>
2022-02-19 13:53:09 -0300

    Particles

---
+++ Assets/ParticlesManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ParticlesManager : MonoBehaviour
+{
+    public GameObject sparklesPrefab; 
+    private GameObject instantiatedSparkles;
+    public void PlayParticles(GameObject obj)
+    {
+        if (obj.GetComponent<Interactable>().undiscovered)
+            instantiatedSparkles = Instantiate(sparklesPrefab, obj.transform.position, Quaternion.identity);
+    }
+
+    public void StopParticles()
+    {
+        if (instantiatedSparkles)
+        {
+            StartCoroutine(DestroyParticles());
+        }
+    }
+
+    private IEnumerator DestroyParticles()
+    {
+        yield return new WaitForSeconds(.4f);
+        instantiatedSparkles.GetComponent<ParticleSystem>().Stop();
+        yield return new WaitForSeconds(2);
+        Destroy(instantiatedSparkles);
+    } 
+}
